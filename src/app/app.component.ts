cdfa7387b5cebb75a71f25eda840a2d02f4c49f9
Vigneshwari Ramamoorthy <vigneshwariibt@gmail.com>
2023-03-23 20:40:28 -0400

    Sample Registration Form
    Angular Template Driven Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-15-template-driven-registration-forms';
+  userName:string ='';
+  emailId:string = '';
+  password:any ='';
+  confirmPassword:any = '';
+  gender:string = '';
+  qualification:string = '';
+
+  onSubmit(form:NgForm) {
+    if( form.valid) {
+       console.log("Form property  ",form);
+       this.userName = form.value.username;
+       this.emailId = form.value.emailid;
+       this.password= form.value.password;
+       this.confirmPassword = form.value.confirmpassword;
+       this.gender = form.value.gender;
+       this.qualification = form.value.degree;
+       console.log("final values==>",this.userName,",",
+       this.emailId,",",
+       this.password,",",
+       this.confirmPassword,",",
+       this.gender,",",
+       this.qualification );
+    }
+  }
 }
